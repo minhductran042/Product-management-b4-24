b78a2aa69ddb947a276c89054e6b7e19b8e42b4a
minductran <minhductran042@gmail.com>
2024-07-05 13:05:26 +0700

    Them danh sach san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req,res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req,res) => {
+
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
     res.render("admin/pages/products/index", {
-        pageTitle: "Quản lí sản phẩm"
+        pageTitle: "Quản lí sản phẩm",
+        products: products
     });
 }
