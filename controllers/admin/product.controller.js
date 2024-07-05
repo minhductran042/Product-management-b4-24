fe8705eb045ad0367afc08115c96d5097d00056f
minductran <minhductran042@gmail.com>
2024-07-05 13:59:55 +0700

    Tinh nang loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,9 +2,16 @@ const Product = require("../../models/product.model");
 
 module.exports.index = async (req,res) => {
 
-    const products = await Product.find({
-        deleted: false
-    });
+    const find = {
+        deleted: false,
+    }
+    // find.status = "active";
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
 
     console.log(products);
 
