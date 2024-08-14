fbca4cac7f61e4e2ebe300151ac4885152e10b60
minductran <minhductran042@gmail.com>
2024-08-14 13:26:37 +0700

    Tao trang ket qua tim kiem

--- routers/client/index.route.js
+++ routers/client/index.route.js
@@ -1,5 +1,6 @@
 const homeRouter = require("./home.route");
 const productRouter = require("./product.route");
+const searchRouter = require("./search.route");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
@@ -7,4 +8,5 @@ module.exports.index = (app) => {
    app.use(categoryMiddleware.category);
    app.use("/", homeRouter);
    app.use("/products",productRouter);
+   app.use("/search",searchRouter);
 }
