90afc19c7e04cc6404acfb8b163e79bd7ae2bf29
minductran <minhductran042@gmail.com>
2024-08-31 01:00:46 +0700

    Them tinh nang cho phan chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,6 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
+
 const socket = io();
 
 // CLIENT_SEND_MESSAGE 
@@ -12,6 +15,7 @@ if(formChat) {
         content: content
       });
       event.target.content.value = "";
+
     }
   })
 }
@@ -39,7 +43,10 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
         <div class = "inner-content">${data.content}</div>
     `;
     const body = document.querySelector(".chat .inner-body");
-    body.appendChild(div);
+
+    body.insertBefore(div,elementListTyping);
+
+    body.scrollTop = bodyChat.scrollHeight;
 });
 
 
@@ -47,3 +54,80 @@ const bodyChat = document.querySelector(".chat .inner-body");
 if(bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight;
 }
+
+
+//Show icon chat
+
+const emojiPicker = document.querySelector('emoji-picker');
+if(emojiPicker) {
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+  emojiPicker.addEventListener('emoji-click',event => {
+    const icon = event.detail.unicode;
+    inputChat.value = inputChat.value + icon;
+  })
+}
+
+  
+
+//End show icon chat 
+
+
+//button-icon
+const buttonIcon = document.querySelector("[button-icon]");
+if(buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.addEventListener("click", () => {
+    tooltip.classList.toggle('shown');
+  });
+}
+
+//end button-icon
+
+
+//Typing
+
+const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+var typingTimeOut;
+if(inputChat) {
+  inputChat.addEventListener("keyup",() => { //event keyup : moi khi nhac tay len
+    socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(typingTimeOut); 
+
+    typingTimeOut = setTimeout(() => {
+      socket.emit("CLIENT_SEND_TYPING","hidden");
+    }, 3000);
+  })
+}
+
+
+//End typing
+
+
+//SERVER RETURN TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+socket.on("SERVER_RETURN_TYPING", (data) => {
+  if(data.type == "show") {
+    const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+    if(!existTyping) {
+      const boxTyping = document.createElement("div");
+      boxTyping.classList.add("box-typing");
+      boxTyping.setAttribute("user-id",data.userId);
+      boxTyping.innerHTML = `
+        <div class="inner-name">${data.fullName}</div>
+        <div class="inner-dots"><span></span><span></span><span></span></div>
+      `;
+      elementListTyping.appendChild(boxTyping);
+    }
+  } else {
+    const boxTypingDelete = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+    if(boxTypingDelete) {
+      elementListTyping.removeChild(boxTypingDelete);
+    }
+  }
+})
+
+
+//END SERVER RETURN TYPING
