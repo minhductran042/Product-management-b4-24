a705f41692a1491a19a01a170d77b7b4684f0458
minductran <minhductran042@gmail.com>
2024-09-07 13:37:57 +0700

    A hủy kết bạn sẽ xóa A khỏi danh sách Lời mời kết bạn của B

--- public/js/users.js
+++ public/js/users.js
@@ -36,7 +36,7 @@ if(listButtonCancelFriend.length > 0) {
 //End Chuc nang huy gui yeu cau
 
 
-//Chuc nang tu choi ket ban
+//Chuc nang tu choi ket ban : lan dau tien vao trang 
 const listButtonRefuseFriend = document.querySelectorAll("[btn-refuse-friend]");
 if(listButtonRefuseFriend.length > 0) {
     listButtonRefuseFriend.forEach(button => {
@@ -44,7 +44,7 @@ if(listButtonRefuseFriend.length > 0) {
             //Viec 1: Xoa class "add" cho box user
             button.closest(".box-user").classList.add("refuse");
 
-            //Viec 2: Gui len server user id 
+            //Viec 2: Gui len server user id userB
             const userIdB = button.getAttribute("btn-refuse-friend");
             socket.emit("CLIENT_REFUSE_FRIEND",  userIdB);
         });
@@ -79,3 +79,91 @@ socket.on("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", (data) => {
     }
 })
   // End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+
+
+// SERVER_RETURN_INFO_ACCEPT_FRIEND
+socket.on("SERVER_RETURN_INFO_ACCEPT_FRIEND", (data) => {
+    const dataUsersAccept = document.querySelector(`[data-users-accept="${data.userIdB}"]`);
+  
+    if(dataUsersAccept) {
+        const boxUserA = document.createElement("div");
+        boxUserA.classList.add("col-6");
+        boxUserA.setAttribute("user-id",data.infoA._id);
+        boxUserA.innerHTML = `
+            <div class="box-user">
+            <div class="inner-avatar">
+                <img src="https://robohash.org/hicveldicta.png" alt="${data.infoA.fullName}">
+            </div>
+            <div class="inner-info">
+                <div class="inner-name">${data.infoA.fullName}</div>
+                <div class="inner-buttons">
+                <button 
+                    class="btn btn-sm btn-primary mr-1" 
+                    btn-accept-friend="${data.infoA._id}"
+                >
+                    Chấp nhận
+                </button>
+                <button 
+                    class="btn btn-sm btn-secondary mr-1" 
+                    btn-refuse-friend="${data.infoA._id}"
+                >
+                    Xóa
+                </button>
+                <button 
+                    class="btn btn-sm btn-secondary mr-1" 
+                    btn-deleted-friend="" disabled=""
+                >
+                    Đã xóa
+                </button>
+                <button 
+                    class="btn btn-sm btn-primary mr-1" 
+                    btn-accepted-friend="" disabled=""
+                >
+                    Đã chấp nhận
+                </button>
+                </div>
+            </div>
+            </div>
+        `;
+  
+        dataUsersAccept.appendChild(boxUserA);
+
+        // Bắt sự kiện cho nút xóa
+        const buttonRefuse = boxUserA.querySelector("[btn-refuse-friend]");
+        buttonRefuse.addEventListener("click", () => {
+            // Việc 1: Thêm class "refuse" cho box-user
+            buttonRefuse.closest(".box-user").classList.add("refuse");
+
+            // Việc 2: Gửi lên server userIdA
+            const userIdA = buttonRefuse.getAttribute("btn-refuse-friend");
+            socket.emit("CLIENT_REFUSE_FRIEND", userIdA);
+        })
+
+        //bat su kien cho nut chap nhan
+        const buttonAccept = boxUserA.querySelector("[btn-accept-friend]");
+        buttonAccept.addEventListener("click", () => {
+            // Việc 1: Thêm class "accepted" cho box-user
+            buttonAccept.closest(".box-user").classList.add("accepted");
+
+            // Việc 2: Gửi lên server userIdA
+            const userIdA = buttonAccept.getAttribute("btn-accept-friend");
+            socket.emit("CLIENT_ACCEPT_FRIEND", userIdA);
+        })
+    }
+
+})
+
+
+
+//SERVER_RETURN_ID_CANCEL_FRIEND
+socket.on("SERVER_RETURN_ID_CANCEL_FRIEND", (data) => {
+   const dataUserAccept = document.querySelector(`[data-users-accept="${data.userIdB}"]`);
+   if(dataUserAccept) {
+        const boxUserA = dataUserAccept.querySelector(`[user-id="${data.userIdA}"]`);
+        if(boxUserA) {
+            dataUserAccept.removeChild(boxUserA);
+        }
+   }
+   
+})
+//End SERVER_RETURN_ID_CANCEL_FRIEND
--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -53,95 +53,103 @@ module.exports = (req,res) => {
                 length: infoB.acceptFriends.length,
                 userId: userIdB
             });
+
+            //Lay thong tin cua A de tra ve cho B
+            const infoA = await User.findOne({
+                _id: userIdA,
+            }).select("id fullName avatar");
+            
+            socket.broadcast.emit("SERVER_RETURN_INFO_ACCEPT_FRIEND", {
+                userIdB: userIdB,
+                infoA: infoA
+            });
+
     })
         //End khi A gui yeu cau cho B
 
 
-        // Chức năng hủy gửi yêu cầu
-        socket.on("CLIENT_CANCEL_FRIEND", async (userIdB) => {
-            // Xóa id của A trong acceptFriends của B
-            const existUserAInB = await User.findOne({
-                _id: userIdB,
-                acceptFriends: userIdA
+       // Chức năng hủy gửi yêu cầu
+    socket.on("CLIENT_CANCEL_FRIEND", async (userIdB) => {
+        // Xóa id của A trong acceptFriends của B
+        const existUserAInB = await User.findOne({
+          _id: userIdB,
+          acceptFriends: userIdA
+        });
+        if(existUserAInB) {
+          await User.updateOne({
+            _id: userIdB
+          }, {
+            $pull: {
+              acceptFriends: userIdA
+            }
+          });
+        }
+
+        // Xóa id của B trong requestFriends của A 
+        const existUserBInA = await User.findOne({
+          _id: userIdA,
+          requestFriends: userIdB
+        });
+        if(existUserBInA) {
+          await User.updateOne({
+            _id: userIdA
+          }, {
+            $pull: {
+              requestFriends: userIdB
+            }
+          });
+        }
+
+        // Trả về cho B độ dài của acceptFriends
+        const infoB = await User.findOne({
+          _id: userIdB
+        });
+        
+        socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
+            length: infoB.acceptFriends.length,
+            userId: userIdB
+        });
+
+        //Tra ve cho B id cua ong A
+        socket.broadcast.emit("SERVER_RETURN_ID_CANCEL_FRIEND", {
+            userIdA: userIdA,
+            userIdB: userIdB
+        });
+      })
+      // Hết Chức năng hủy gửi yêu cầu
+
+
+      socket.on("CLIENT_REFUSE_FRIEND", async (userIdB) => {
+        // Xóa id của B trong acceptFriends của A
+        const existUserBInA = await User.findOne({
+            _id: userIdA,
+            acceptFriends: userIdB
+        });
+        if(existUserBInA) {
+            await User.updateOne({
+                _id: userIdA
+            }, {
+                $pull: {
+                    acceptFriends: userIdB
+                }
             });
-    
-            if(existUserAInB) {
+        }
+        // Xóa id của A trong requestFriends của B
+        const existUserAInB = await User.findOne({
+            _id: userIdB,
+            requestFriends: userIdA
+        });
+        if(existUserAInB) {
             await User.updateOne({
                 _id: userIdB
             }, {
                 $pull: {
-                    acceptFriends: userIdA
+                    requestFriends: userIdA
                 }
             });
-            }
-    
-            // Xóa id của B trong requestFriends của A 
-            const existUserBInA = await User.findOne({
-                _id: userIdA,
-                requestFriends: userIdB
-            });
-    
-            if(existUserBInA) {
-                await User.updateOne({
-                    _id: userIdA
-                }, {
-                    $pull: {
-                        requestFriends: userIdB
-                    }
-                });
-            }
-
-            const infoB = await User.findOne({
-                _id: userIdB
-              });
-        
-            socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
-                length: infoB.acceptFriends.length,
-                userId: userIdB
-            });
-    
-        })
-        // Hết Chức năng hủy gửi yêu cầu
-
-
-        //Chuc nang tu choi ket ban
-        socket.on("CLIENT_REFUSE_FRIEND", async (userIdB) => {
-
-            // Xóa id của B trong acceptFriends của A
-            const existUserBInA = await User.findOne({
-                _id: userIdA,
-                acceptFriends: userIdB
-            });
-    
-            if(existUserBInA) {
-                await User.updateOne({
-                    _id: userIdA
-                }, {
-                    $pull: {
-                        acceptFriends: userIdB
-                    }
-                });
-            }
-    
-            // Xóa id của A trong requestFriends của B
-            const existUserAInB = await User.findOne({
-                _id: userIdB,
-                requestFriends: userIdA
-            });
-    
-            if(existUserAInB) {
-                await User.updateOne({
-                    _id: userIdB
-                }, {
-                    $pull: {
-                        requestFriends: userIdA
-                    }
-                });
-            }
-    
-        })
-        //End chuc nang tu choi ket ban
-
+        }
+      })
+      // Hết Chức năng từ chối kết bạn
 
         //Chuc nang chap nhan ket ban
         socket.on("CLIENT_ACCEPT_FRIEND", async (userIdB) => {
