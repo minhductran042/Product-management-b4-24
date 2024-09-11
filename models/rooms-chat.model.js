00ffb1677ded4c51d31794a34a80ef45a7ca8aa6
minductran <minhductran042@gmail.com>
2024-09-11 13:07:50 +0700

    Tao collection rooms-chat

---
+++ models/rooms-chat.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const roomChatSchema = new mongoose.Schema({
+    title: String,
+    //avatar: String,
+    typeRoom: String,
+    users: [
+        {
+            userId: String,
+            role: String
+        }  
+    ],
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+
+  timestamps: true
+
+});
+
+const RoomChat = mongoose.model("RoomChat", cartSchema, "rooms-chat");
+
+module.exports = RoomChat;
